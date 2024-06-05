c2cb919ab1ec5b0f2986edd28d26412428997b82
nehashah4590 <ns457989@gmail.com>
2024-06-05 18:53:21 +0545

    added sort and search functionality in customer table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { useState} from "react";
+import { useState } from "react";
 import CustomersLayout from "./components/customers/Layout";
 import OrderLayout from "./components/orders/Layout";
 import LoyaltyProgramLayout from "./components/loyaltyPrograms/Layout";
 import ExpensesLayout from "./components/expenses/Layout";
 import AddCustomerLoyaltyTrans from "./components/customersLoyaltyrans/AddCustomerLoyaltyTrans";
 import { GoPeople } from "react-icons/go";
-import { MdProductionQuantityLimits , MdLoyalty } from "react-icons/md";
+import { MdProductionQuantityLimits, MdLoyalty } from "react-icons/md";
 import { GiExpense } from "react-icons/gi";
 import { FaMedal } from "react-icons/fa";
 
@@ -28,43 +28,44 @@ function App() {
   return (
     <div className="flex">
       <div className="bg-blue-900 w-[20vw] h-screen py-8 ">
-        <button 
-        className="px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex"
-        onClick={() => handleToggle(setAddCustomer)}
+        <button
+          className={`px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex ${addCustomer ? 'bg-green-400' : 'bg-transparent'}`}
+          onClick={() => handleToggle(setAddCustomer)}
         >
-         <GoPeople className="mr-4 text-2xl" /> Customers
+          <GoPeople className="mr-4 text-2xl" /> Customers
         </button>
-        <button 
-        className="px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex "
-        onClick={() => handleToggle(setAddCustomerOrder)}
+        <button
+          className={`px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex ${addCustomerOrder ? 'bg-green-400' : 'bg-transparent'}`}
+          onClick={() => handleToggle(setAddCustomerOrder)}
         >
-          <MdProductionQuantityLimits className="mr-4 text-2xl"/>Orders
+          <MdProductionQuantityLimits className="mr-4 text-2xl" />Orders
         </button>
-        <button 
-        className="px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex "
-        onClick={() => handleToggle(setAddCustomerLoyaltyTrans)}
+        <button
+          className={`px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex ${addCustomerLoyaltyTrans ? 'bg-green-400' : 'bg-transparent'}`}
+          onClick={() => handleToggle(setAddCustomerLoyaltyTrans)}
         >
-          <FaMedal className="mr-4 text-2xl"/>Loyalty Transactions
+          <FaMedal className="mr-4 text-2xl" />Loyalty Transactions
         </button>
-        <button 
-        className="px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex "
-        onClick={() => handleToggle(setAddLoyaltyProgram)}
+        <button
+          className={`px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex ${addLoyaltyProgram ? 'bg-green-400' : 'bg-transparent'}`}
+
+          onClick={() => handleToggle(setAddLoyaltyProgram)}
         >
-          <MdLoyalty className="mr-4 text-2xl"/>Loyalty Programs
+          <MdLoyalty className="mr-4 text-2xl" />Loyalty Programs
         </button>
-        <button 
-        className="px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex "
-        onClick={() => handleToggle(setAddExpenses)}
+        <button
+          className={`px-8 font-semibold py-4 mt-8 w-full hover:bg-green-400 text-white flex ${addExpenses ? 'bg-green-400' : 'bg-transparent'}`}
+          onClick={() => handleToggle(setAddExpenses)}
         >
-          <GiExpense className="mr-4 text-2xl"/>Expenses
+          <GiExpense className="mr-4 text-2xl" />Expenses
         </button>
       </div>
       <div className="w-full bg-gray-200">
-        {addCustomer && <CustomersLayout/>}
-        {addCustomerOrder && <OrderLayout/>}
-        {addLoyaltyProgram && <LoyaltyProgramLayout/>}
-        {addExpenses && <ExpensesLayout/>}
-        {addCustomerLoyaltyTrans && <AddCustomerLoyaltyTrans/>}
+        {addCustomer && <CustomersLayout />}
+        {addCustomerOrder && <OrderLayout />}
+        {addLoyaltyProgram && <LoyaltyProgramLayout />}
+        {addExpenses && <ExpensesLayout />}
+        {addCustomerLoyaltyTrans && <AddCustomerLoyaltyTrans />}
       </div>
     </div>
   );
--- src/components/customers/UpdateCustomersDetails.jsx
+++ src/components/customers/UpdateCustomersDetails.jsx
@@ -1,61 +1,110 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { MdSearch } from "react-icons/md";
 
 const UpdateCustomersDetails = () => {
-    const [customersData, setCustomersData] = useState([]);
-    const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [customersData, setCustomersData] = useState([]);
+  const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [order, setOrder] = useState("ASC");
+  const [searchQuery, setSearchQuery] = useState('');
 
-    useEffect(() => {
-      // Fetch customer data from the backend
-      axios.get(`${process.env.REACT_APP_API_HOST}/customers`)
-        .then(response => {
-            setCustomersData(response.data.customerData);
-        })
-        .catch(error => {
-          console.error('Error fetching customers:', error);
-        });
-    }, []);
-  
+  useEffect(() => {
+    // Fetch customer data from the backend
+    axios.get(`${process.env.REACT_APP_API_HOST}/customers`)
+      .then(response => {
+        setCustomersData(response.data.customerData);
+        setFilteredData(response.data.customerData);
+      })
+      .catch(error => {
+        console.error('Error fetching customers:', error);
+      });
+  }, []);
 
-    const updatePhoneNumber = (customerId) => {
-      // Make API request to update phone number
-      console.log(customerId)
-      axios.put(`${process.env.REACT_APP_API_HOST}/customers`, { 
-        customer_id:customerId,
-        phone:newPhoneNumber })
-        .then(response => {
-          // Update local state with updated customer data
-          console.log(response);
-          
-        })
-        .catch(error => {
-          console.error('Error updating phone number:', error);
-        });
+  useEffect(() => {
+    const result = customersData.filter(customer => {
+      return Object.values(customer).some(value =>
+        value.toString().toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    });
+    setFilteredData(result);
+  }, [searchQuery]);
+
+  const sorting = (col) => {
+    const compare = (a, b) => {
+      const valA = a[col];
+      const valB = b[col];
+
+      if (typeof valA === 'string' && typeof valB === 'string') {
+        return order === "ASC"
+          ? valA.toLowerCase() > valB.toLowerCase() ? 1 : -1
+          : valA.toLowerCase() < valB.toLowerCase() ? 1 : -1;
+      } else if (typeof valA === 'number' && typeof valB === 'number') {
+        return order === "ASC"
+          ? valA - valB
+          : valB - valA;
+      } else {
+        // Handle cases where types are mixed or not string/number
+        return 0;
+      }
     };
-  
-    return (
-      <div className='mb-4 m-10'>
-        <h1 className="text-2xl font-bold ">Customer Details</h1>
-        <div className="">
-            <div className='flex items-center border border-gray-300 p-2 space-x-2 bg-white rounded-md shadow-md font-bold '>
-                <p className='w-[50px] bg-gray-200 rounded-md px-4 py-2'>ID</p>
-                <p className='w-[200px] bg-gray-200 rounded-md px-4 py-2'>Name</p>
-                <p className='w-[300px] bg-gray-200 rounded-md px-4 py-2'>Email</p>
-                <p className='w-[100px] bg-gray-200 rounded-md px-4 py-2'>Phone</p>
-            </div>
-          {customersData.map(customer => (
-            <div key={customer.customer_id} className="flex items-center border border-gray-300 p-2 space-x-2 bg-white rounded-md shadow-md">
-              <div className='w-[50px] overflow-x-auto'>{customer.customer_id}</div>
-              <div className='w-[100px] overflow-x-auto'>{customer.first_name}</div>
-              <div className='w-[100px] overflow-x-auto'>{customer.last_name}</div>
-              <div className='w-[300px] overflow-x-auto'>{customer.email}</div>
-              <input className='w-[100px]'  type="text" defaultValue={customer.phone} onChange={(e)=>setNewPhoneNumber(e.target.value)}></input>
-              <button onClick={() => updatePhoneNumber(customer.customer_id)} className="ml-2 bg-gray-600 text-white px-4 py-2 rounded-md hover:bg-green-400">Update</button>
-            </div>
-          ))}
-        </div>
+    const sorted = [...customersData].sort(compare);
+    setFilteredData(sorted);
+    setOrder(order === "ASC" ? "DSC" : "ASC");
+  };
+
+
+
+  const updatePhoneNumber = (customerId) => {
+    // Make API request to update phone number
+    console.log(customerId)
+    axios.put(`${process.env.REACT_APP_API_HOST}/customers`, {
+      customer_id: customerId,
+      phone: newPhoneNumber
+    })
+      .then(response => {
+        // Update local state with updated customer data
+        console.log(response);
+
+      })
+      .catch(error => {
+        console.error('Error updating phone number:', error);
+      });
+  };
+
+  return (
+
+    <div className='mb-4 m-10'>
+      <div className='flex justify-center relative'>
+        <input
+          type="text"
+          placeholder="Search customers"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+          className='px-4 py-2 rounded-md shadow-md w-[400px] '
+          /><MdSearch  className=' absolute right-[200px] mt-1 text-3xl'/>
       </div>
-    );
+      <h1 className="text-2xl font-bold ">Customer Details</h1>
+      <div className="">
+        <div className='flex items-center border border-gray-300 p-2 space-x-2 bg-white rounded-md shadow-md font-bold '>
+          <p onClick={() => sorting("customer_id")} className='w-[50px] bg-gray-200 rounded-md px-4 py-2'>ID</p>
+          <p onClick={() => sorting("first_name")} className='w-[200px] bg-gray-200 rounded-md px-4 py-2'>Name</p>
+          <p onClick={() => sorting("email")} className='w-[300px] bg-gray-200 rounded-md px-4 py-2'>Email</p>
+          <p onClick={() => sorting("phone")} className='w-[100px] bg-gray-200 rounded-md px-4 py-2'>Phone</p>
+        </div>
+        {filteredData.map(customer => (
+          <div key={customer.customer_id} className="flex items-center border border-gray-300 p-2 space-x-2 bg-white rounded-md shadow-md">
+            <div className='w-[50px] overflow-x-auto'>{customer.customer_id}</div>
+            <div className='w-[100px] overflow-x-auto'>{customer.first_name}</div>
+            <div className='w-[100px] overflow-x-auto'>{customer.last_name}</div>
+            <div className='w-[300px] overflow-x-auto'>{customer.email}</div>
+            <input className='w-[100px]' type="text" defaultValue={customer.phone} onChange={(e) => setNewPhoneNumber(e.target.value)}></input>
+            <button onClick={() => updatePhoneNumber(customer.customer_id)} className="ml-2 bg-gray-600 text-white px-4 py-2 rounded-md hover:bg-green-400">Update</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default UpdateCustomersDetails
